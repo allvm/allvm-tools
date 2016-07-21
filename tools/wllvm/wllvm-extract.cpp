18704ee14230bc53b5f5615625a60756e05001d7
Will Dietz <w@wdtz.org>
2016-07-21 10:53:16 -0500

    wllvm-extract: Write linked bitcode to file, default to input + .bc

--- tools/wllvm/wllvm-extract.cpp
+++ tools/wllvm/wllvm-extract.cpp
@@ -11,6 +11,7 @@
 #include "Error.h"
 #include "WLLVMFile.h"
 
+#include <llvm/Bitcode/ReaderWriter.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IRReader/IRReader.h>
@@ -19,6 +20,7 @@
 #include <llvm/Support/PrettyStackTrace.h>
 #include <llvm/Support/Signals.h>
 #include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/ToolOutputFile.h>
 #include <llvm/Support/raw_os_ostream.h>
 
 using namespace allvm;
@@ -27,6 +29,10 @@ using namespace llvm;
 static cl::opt<std::string>
     InputFilename(cl::Positional, cl::Required,
                   cl::desc("<input file built with wllvm>"));
+
+static cl::opt<std::string>
+OutputFilename("o", cl::desc("Override output filename"),
+               cl::value_desc("filename"));
 
 int main(int argc, const char **argv, const char **envp) {
   sys::PrintStackTraceOnErrorSignal(argv[0]);
@@ -38,6 +44,21 @@ int main(int argc, const char **argv, const char **envp) {
 
   // Open the specified file
   auto WLLVMFile = WLLVMFile::open(InputFilename);
+
+  // Initialize output file, error early if unavailable
+  if (OutputFilename.empty()) {
+    if (StringRef(InputFilename) != "-")
+      OutputFilename = InputFilename + ".bc";
+  }
+  std::unique_ptr<tool_output_file> Out;
+  std::error_code EC;
+  Out.reset(new tool_output_file(OutputFilename, EC, sys::fs::F_None));
+  if (EC) {
+    errs() << "Error opening file '" << OutputFilename << "': ";
+    errs() << EC.message() << "\n";
+    errs().flush();
+    return 1;
+  }
 
   // Create an empty module to link these into
   auto Composite = make_unique<Module>("linked", C);
@@ -54,8 +75,10 @@ int main(int argc, const char **argv, const char **envp) {
       reportError(BCFilename, "error linking module");
   }
 
-  // TODO: Do something more useful than dump :)
-  Composite->dump();
+  WriteBitcodeToFile(Composite.get(), Out->os());
+
+  // We made it this far without error, keep the result.
+  Out->keep();
 
   return 0;
 }
