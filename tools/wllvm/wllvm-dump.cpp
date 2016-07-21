13f2fccd136b767e8a8ec55cc23ec66db7c52b39
Will Dietz <w@wdtz.org>
2016-07-21 08:55:43 -0500

    wllvm-dump: clang-format

--- tools/wllvm/wllvm-dump.cpp
+++ tools/wllvm/wllvm-dump.cpp
@@ -13,10 +13,10 @@
 #include <llvm/Object/Archive.h>
 #include <llvm/Object/ObjectFile.h>
 #include <llvm/Support/CommandLine.h>
-#include <llvm/Support/raw_os_ostream.h>
 #include <llvm/Support/LineIterator.h>
 #include <llvm/Support/PrettyStackTrace.h>
 #include <llvm/Support/Signals.h>
+#include <llvm/Support/raw_os_ostream.h>
 
 using namespace allvm;
 using namespace llvm;
@@ -24,13 +24,14 @@ using namespace object;
 
 const StringRef WLLVMSectionName = ".llvm_bc";
 
-static cl::opt<std::string> InputFilename(
-  cl::Positional, cl::Required, cl::desc("<input file built with wllvm>"));
+static cl::opt<std::string>
+    InputFilename(cl::Positional, cl::Required,
+                  cl::desc("<input file built with wllvm>"));
 
 int main(int argc, const char **argv, const char **envp) {
   sys::PrintStackTraceOnErrorSignal(argv[0]);
   PrettyStackTraceProgram X(argc, argv);
-  llvm_shutdown_obj Y;  // Call llvm_shutdown() on exit.
+  llvm_shutdown_obj Y; // Call llvm_shutdown() on exit.
   cl::ParseCommandLineOptions(argc, argv);
 
   // Open the specified file
@@ -40,22 +41,22 @@ int main(int argc, const char **argv, const char **envp) {
   auto &Binary = *BinaryOrErr.get().getBinary();
 
   // Check the filetype, later we might want to support more of these.
-  auto error = [](StringRef Msg) {
-    reportError(InputFilename, Msg);
-  };
+  auto error = [](StringRef Msg) { reportError(InputFilename, Msg); };
   if (Archive *a = dyn_cast<Archive>(&Binary))
     error("archives are not yet supported");
   ObjectFile *o = dyn_cast<ObjectFile>(&Binary);
   if (!o)
-    error("invalid or unsupported file format, unable to read as an object file");
+    error(
+        "invalid or unsupported file format, unable to read as an object file");
   if (!o->isELF())
     error("not an ELF object file");
 
   // Find the section and print its contents
-  for (auto &S: o->sections()) {
+  for (auto &S : o->sections()) {
     StringRef Name;
     auto check = [](std::error_code ec) {
-      if (ec) reportError(InputFilename, ec);
+      if (ec)
+        reportError(InputFilename, ec);
     };
     check(S.getName(Name));
 
