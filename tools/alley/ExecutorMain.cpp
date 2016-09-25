c20311520fe4e6daecb1fdc8ea11e665cb14dd19
Vikram S. Adve <vadve@illinois.edu>
2016-09-25 15:52:07 -0500

    First working version of static code gen path: * Retrieves cached binary code as a file descriptor, not MemoryBuffer.   This also means we no longer need to copy into a shmem region or tmpfs. * If not in cache, compiles and caches the binary via a temp file   FIXME: Avoid 'tmpnam': may need to update the StaticCodeGen interface * Add a debug option: "alley" * Add option "DoStaticCodeGen" to force static code gen on a cache miss * Corrected AddProgName() to make a copy of InputFileName before modifying it * Deleted a bunch of unused code from the shmem attempt: file desc is better

--- tools/alley/ExecutorMain.cpp
+++ tools/alley/ExecutorMain.cpp
@@ -18,6 +18,17 @@
 #include <llvm/Support/TargetRegistry.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Support/raw_os_ostream.h>
+#include <llvm/Support/Debug.h>
+
+#define DEBUG_TYPE "alley"
+
+#include <unistd.h>
+//#include <types.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <iostream>
 
 using namespace allvm;
 using namespace llvm;
@@ -41,9 +52,13 @@ static cl::opt<std::string> InputFilename(cl::Positional, cl::Required,
 static cl::list<std::string> InputArgv(cl::ConsumeAfter,
                                        cl::desc("<program arguments>..."));
 
-static cl::opt<bool> EnableJIT(
-    "enableJIT", cl::init(true),
+static cl::opt<bool> DisableJIT(
+    "disableJIT", cl::init(false),
     cl::desc("Choose between JIT or static compilation (default: JIT)"));
+
+static cl::opt<bool> DoStaticCodeGen(
+    "doCodeGen", cl::init(true),
+    cl::desc("Do static native code generation if necessary (default: ON)"));
 
 const StringRef ALLEXE_MAIN = "main.bc";
 
@@ -68,12 +83,25 @@ int main(int argc, const char **argv, const char **envp) {
   }
 
   // Choose the code generation mode Dynamic (using JIT) or Static
-  if (EnableJIT) {
-    return execWithJITCompilation(allexe, envp);
-  } else {
+  if (DisableJIT) {
     return execWithStaticCompilation(allexe, envp);
+  } else {
+    return execWithJITCompilation(allexe, envp);
   }
 }
+
+
+// Helper function for execWith{JIT,Static}Compilation:
+// Add name of file as argv[0]
+void AddProgName()
+{
+  std::string theName(InputFilename.getValue()); // make a copy to edit
+  StringRef ProgName = theName;	// wrap in a StringRef for easier operations
+  if (sys::path::has_extension(InputFilename))
+    ProgName = ProgName.drop_back(sys::path::extension(ProgName).size());
+  InputArgv.insert(InputArgv.begin(), ProgName);
+}
+
 
 /****************************************************************
  * Name:        execWithJITCompilation
@@ -118,29 +146,32 @@ int execWithJITCompilation(allvm::Allexe &allexe, const char **envp) {
   }
 
   // Add name of file as argv[0]
-  StringRef ProgName = InputFilename;
-  if (sys::path::has_extension(InputFilename))
-    ProgName = ProgName.drop_back(sys::path::extension(ProgName).size());
-  InputArgv.insert(InputArgv.begin(), ProgName);
+  AddProgName();
 
   return executor->runHostedBinary(InputArgv, envp, LibNone);
 }
+
 
 /****************************************************************
  * Name:        execWithStaticCompilation
  *
  * Input:       A source file in allexe format & program’s environment.
  *
- * Output:      Compiles the 'single' module embedded in allexe and creates
- *              an object file out of it.
+ * Output:      Uses the StaticBinaryCache to look up and execute the native
+ *              object code for the given .allexe program.  If the code is
+ *              not available in the cache, this should return an error, but
+ *              (for convenience and for experiments) will compile the 
+ *              module embedded in allexe to create an object file, and then
+ *              notifies the cache to save it before executing it. 
  *
  * Assumptions: The allexe embeds a single module and is obtained
  *              by tool like alltogether. The key for getting a single
- *              module is to merge all the librraies with the main module
+ *              module is to merge all the libraries with the main module,
  *              which is what alltogether does.
  ****************************************************************/
+
 int execWithStaticCompilation(allvm::Allexe &allexe,
-                              LLVM_ATTRIBUTE_UNUSED const char **envp) {
+                              const char **envp) {
 
   assert(allexe.getNumModules() == 1 &&
          "The input must be an allexe with a single module");
@@ -160,10 +191,11 @@ int execWithStaticCompilation(allvm::Allexe &allexe,
   auto name = allexe.getModuleName(0);
   if (!M) {
     errs() << "Could not read " << InputFilename << ": " << name << "\n";
-    // XXX: Do something with M's error
+    // FIXME: Do something with M's error
     exit(1);
   }
 
+  // Set a unique name for the module using StaticBinaryCache's naming scheme
   const CompilationOptions Options;
   M.get()->setModuleIdentifier(
       StaticBinaryCache::generateName(name, crc, &Options));
@@ -171,28 +203,58 @@ int execWithStaticCompilation(allvm::Allexe &allexe,
   // Setting Up the Cache
   std::unique_ptr<StaticBinaryCache> Cache(new StaticBinaryCache());
 
-  // Query the cache before the static compilation for an existing Image
+  // Query the cache for an existing Image, which would avoid native code-gen
   Module *Mod = M.get().get();
-  bool isCached = Cache->hasObjectFor(Mod);
-  llvm::errs() << "\nisPresent:" << isCached << "\n";
+  int execFD = Cache->getObjectFileDesc(Mod);
+  bool isCached = (execFD >= 0);
 
-  if (isCached) {
-    // Cache Hit; Execute
-    return 0;
+  DEBUG(dbgs() << (isCached? "Found in cache\n" : "Not in cache!\n"));
+
+  if (! isCached && DoStaticCodeGen) {
+    // Generate native code for the specified .allexe into a temp file.
+    // C++ doesn't have a portable way to create a temp file: using a C idiom.
+    // FIXME: tmpnam() should not be used because there is a small chance
+    // the temp file is created by another process or thread before being 
+    // opened by us, but that cannot be fixed here: the real problem
+    // is that StaticCodeGen() wants a file name and then writes to it,
+    // creating an opening for the race condition.  Instead, it should have
+    // an option to leave the file name unspecified and create it internally.
+    // 
+    char tempFileName[L_tmpnam];
+    (void) tmpnam(tempFileName);
+    ErrorOr<std::unique_ptr<object::Binary>> binary =
+      compileAndLinkAllexeWithLlcDefaults(allexe, LibNone, "clang",
+					  tempFileName, context);
+    if (! binary) {
+      errs() << "Compile/link failed for allexe " 
+	     << allexe.getModuleName(0) << "\n";
+      return -1;
+    }
+    DEBUG(dbgs() << "Compiled successfully into " << tempFileName << "\n");
+
+    // Now copy the executable to the cache location and delete the temp file
+    Cache->notifyObjectCompiled(Mod, tempFileName);
+    sys::fs::remove(Twine(tempFileName));
+    execFD = Cache->getObjectFileDesc(Mod);
+    assert(execFD != -1 && "Failed to retrieve native code from cache!");
   }
 
-  // The following is just for testing the current implementation of caching.
-  llvm::ErrorOr<std::unique_ptr<object::ObjectFile>> ObjFile =
-      compileAllexe(allexe, "test.out", Options, context);
+  // Add the name of the allexe file (without the extension) as InputArgv[0].
+  // Do NOT use the native code name: that is not a meaningful name.
+  // 
+  AddProgName();
+  
+  // Convert InputArgv[] from cl::list<std::string> to char*[].
+  // This is all so low-level ... need an equivalent of execve in llvm::sys.
+  char** argvArray = (char**) malloc((InputArgv.size() + 1) * sizeof(char*));
+  for (int i=0, e = InputArgv.size(); i < e; i++)
+    argvArray[i] = (char*) InputArgv[i].c_str();
+  argvArray[InputArgv.size()] = (char*) 0; // null-terminate argvArray[]
 
-  ErrorOr<std::unique_ptr<MemoryBuffer>> FileOrErr =
-      MemoryBuffer::getFile("test.out");
-  if (std::error_code EC = FileOrErr.getError()) {
-    errs() << " Error reading object file to buffer '" << ALLEXE_MAIN << "'\n";
-    return 1;
-  }
-  std::unique_ptr<MemoryBuffer> Buffer = std::move(FileOrErr.get());
-  Cache->notifyObjectCompiled(Mod, Buffer->getMemBufferRef());
+  // Almost ready to launch this sucker
+  DEBUG(dbgs() << "fexecve: " << execFD << ": " << argvArray[0] << "\n");
+  fexecve(execFD, argvArray, const_cast<char**>(envp));
 
-  return 0;
+  perror("fexecve failed!");	// fexecve never returns if successful!
+  return -1;
 }
