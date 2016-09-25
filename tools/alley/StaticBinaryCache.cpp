7337363953951ae7bdbbb81c143881fd5ee5bb56
Vikram S. Adve <vadve@illinois.edu>
2016-09-25 16:20:11 -0500

    clang-format these files.

--- tools/alley/StaticBinaryCache.cpp
+++ tools/alley/StaticBinaryCache.cpp
@@ -4,16 +4,16 @@
 #include <llvm/ADT/StringExtras.h>
 #include <llvm/IR/Module.h>
 #include <llvm/Object/Binary.h>
+#include <llvm/Support/Debug.h>
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/raw_ostream.h>
-#include <llvm/Support/Debug.h>
 
 #define DEBUG_TYPE "cache"
 
-#include <zlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <zlib.h>
 
 using namespace llvm;
 
@@ -43,7 +43,7 @@ StaticBinaryCache::StaticBinaryCache() {
 }
 
 std::string StaticBinaryCache::getAndResetCacheFile(const Module *M,
-						      bool doReset=true) {
+                                                    bool doReset = true) {
   const std::string &ModuleID = M->getModuleIdentifier();
   std::string CacheName;
   if (!getCacheFilename(ModuleID, CacheName))
@@ -61,9 +61,9 @@ std::string StaticBinaryCache::getAndResetCacheFile(const Module *M,
 // Copy binary code from a MemoryBuffer to the cache location
 void StaticBinaryCache::notifyObjectCompiled(const Module *M,
                                              MemoryBufferRef Obj) {
-  std::string CacheName = getAndResetCacheFile(M, /*doReset*/true);
+  std::string CacheName = getAndResetCacheFile(M, /*doReset*/ true);
   DEBUG(dbgs() << "Caching MemoryBuffer for " << M->getModuleIdentifier()
-	<< " at " << CacheName << "\n");
+               << " at " << CacheName << "\n");
   std::error_code EC;
   raw_fd_ostream outfile(CacheName, EC, sys::fs::F_None);
   outfile.write(Obj.getBufferStart(), Obj.getBufferSize());
@@ -73,10 +73,10 @@ void StaticBinaryCache::notifyObjectCompiled(const Module *M,
 // Copy binary code from an executable file to the cache location.
 // execFilePath must be a valid path to a readable file.
 void StaticBinaryCache::notifyObjectCompiled(const Module *M,
-					     const std::string& execFilePath) {
-  std::string CacheName = getAndResetCacheFile(M, /*doReset*/false);
-  DEBUG(dbgs() << "Copying binary for " << M->getModuleIdentifier()
-	<< " from " << execFilePath << " to " << CacheName << "\n");
+                                             const std::string &execFilePath) {
+  std::string CacheName = getAndResetCacheFile(M, /*doReset*/ false);
+  DEBUG(dbgs() << "Copying binary for " << M->getModuleIdentifier() << " from "
+               << execFilePath << " to " << CacheName << "\n");
   std::error_code EC = sys::fs::rename(execFilePath, Twine(CacheName));
   if (EC.value() < 0)
     errs() << "StaticBinaryCache: file copy failed: " << EC.message() << "\n";
@@ -116,7 +116,8 @@ bool StaticBinaryCache::hasObjectFor(const Module *M) {
   return !!IRObjectBuffer;
 }
 
-std::unique_ptr<MemoryBuffer> StaticBinaryCache::getObject(StringRef Name) const {
+std::unique_ptr<MemoryBuffer>
+StaticBinaryCache::getObject(StringRef Name) const {
   std::string CacheName;
   if (!getCacheFilename(Name, CacheName))
     return nullptr;
@@ -143,12 +144,12 @@ int StaticBinaryCache::getObjectFileDesc(StringRef Name) const {
   DEBUG(dbgs() << "Exec cache file name: " << CacheName << "\n");
 
   // Open the cache file in read-only mode and return the file descriptor.
-  int execFD = open(CacheName.c_str(), S_IRUSR/*perm: 00400*/);
-  if (execFD == -1) {		// Not an error: just a cache miss
-    DEBUG(dbgs() << "Executable does not exist in file cache "
-	  << CacheDir << "\n");
+  int execFD = open(CacheName.c_str(), S_IRUSR /*perm: 00400*/);
+  if (execFD == -1) { // Not an error: just a cache miss
+    DEBUG(dbgs() << "Executable does not exist in file cache " << CacheDir
+                 << "\n");
   }
-  
+
   return execFD;
 }
 
@@ -169,8 +170,8 @@ bool StaticBinaryCache::getCacheFilename(StringRef ModID,
   CacheName = CacheDir + CacheSubdir;
   size_t pos = CacheName.rfind('.');
   CacheName.replace(pos, CacheName.length() - pos, "");
-  DEBUG(dbgs() << "For Module " << ModID << 
-	" : Cache file name: " << CacheName << "\n");
+  DEBUG(dbgs() << "For Module " << ModID << " : Cache file name: " << CacheName
+               << "\n");
   return true;
 }
 
