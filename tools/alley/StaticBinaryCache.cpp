9bb624e27c85e91d57c1b37a29195abd0be0c107
Vikram S. Adve <vadve@illinois.edu>
2016-09-25 15:37:13 -0500

    Add methods to get cached object as a file descriptor. Mark getObject and getCacheFileName methods const, except the one we override from the llvm::ImageCache.

--- tools/alley/StaticBinaryCache.cpp
+++ tools/alley/StaticBinaryCache.cpp
@@ -3,10 +3,17 @@
 #include <llvm/ADT/SmallString.h>
 #include <llvm/ADT/StringExtras.h>
 #include <llvm/IR/Module.h>
+#include <llvm/Object/Binary.h>
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/Debug.h>
+
+#define DEBUG_TYPE "cache"
 
 #include <zlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/fcntl.h>
 
 using namespace llvm;
 
@@ -35,39 +42,81 @@ StaticBinaryCache::StaticBinaryCache() {
   }
 }
 
-void StaticBinaryCache::notifyObjectCompiled(const Module *M,
-                                             MemoryBufferRef Obj) {
+std::string StaticBinaryCache::getAndResetCacheFile(const Module *M,
+						      bool doReset=true) {
   const std::string &ModuleID = M->getModuleIdentifier();
   std::string CacheName;
   if (!getCacheFilename(ModuleID, CacheName))
-    return;
+    return std::string("");
   if (!CacheDir.empty()) { // Create user-defined cache dir.
     SmallString<128> dir(sys::path::parent_path(CacheName));
     sys::fs::create_directories(Twine(dir));
   }
+  // Delete existing file with this name, if any, because it is out of date
+  if (doReset)
+    sys::fs::remove(Twine(CacheName)); // safe even if file does not exist.
+  return CacheName;
+}
+
+// Copy binary code from a MemoryBuffer to the cache location
+void StaticBinaryCache::notifyObjectCompiled(const Module *M,
+                                             MemoryBufferRef Obj) {
+  std::string CacheName = getAndResetCacheFile(M, /*doReset*/true);
+  DEBUG(dbgs() << "Caching MemoryBuffer for " << M->getModuleIdentifier()
+	<< " at " << CacheName << "\n");
   std::error_code EC;
   raw_fd_ostream outfile(CacheName, EC, sys::fs::F_None);
   outfile.write(Obj.getBufferStart(), Obj.getBufferSize());
   outfile.close();
 }
 
+// Copy binary code from an executable file to the cache location.
+// execFilePath must be a valid path to a readable file.
+void StaticBinaryCache::notifyObjectCompiled(const Module *M,
+					     const std::string& execFilePath) {
+  std::string CacheName = getAndResetCacheFile(M, /*doReset*/false);
+  DEBUG(dbgs() << "Copying binary for " << M->getModuleIdentifier()
+	<< " from " << execFilePath << " to " << CacheName << "\n");
+  std::error_code EC = sys::fs::rename(execFilePath, Twine(CacheName));
+  if (EC.value() < 0)
+    errs() << "StaticBinaryCache: file copy failed: " << EC.message() << "\n";
+}
+
+#if 0
+void StaticBinaryCache::notifyObjectCompiled(const Module *M,
+			     std::unique_ptr<object::Binary> binary) {
+  std::string CacheName = getAndResetCacheFile(M);
+  DEBUG(dbgs() << "Caching binary for " << M->getModuleIdentifier()
+	<< " at " << CacheName << "\n");
+  MemoryBufferRef Obj = binary.get()->getMemoryBufferRef();
+  std::error_code EC;
+  raw_fd_ostream outfile(CacheName, EC, sys::fs::F_None);
+  outfile.write(Obj.getBufferStart(), Obj.getBufferSize());
+  outfile.close();
+}
+#endif
+
 std::unique_ptr<MemoryBuffer> StaticBinaryCache::getObject(const Module *M) {
   return getObject(M->getModuleIdentifier());
 }
 
+int StaticBinaryCache::getObjectFileDesc(const Module *M) const {
+  return getObjectFileDesc(M->getModuleIdentifier());
+}
+
 bool StaticBinaryCache::hasObjectFor(const Module *M) {
-  // TODO: Avoid loading file altogether!
   std::string CacheName;
+  // FIXME: Avoid loading file altogether!
   if (!getCacheFilename(M->getModuleIdentifier(), CacheName))
     return false;
   // Load the object from the cache filename
   ErrorOr<std::unique_ptr<MemoryBuffer>> IRObjectBuffer =
       MemoryBuffer::getFile(CacheName.c_str(), -1, false);
-  // If the file isn't there, that's OK.
+  // Return true if the file is there
   return !!IRObjectBuffer;
 }
 
-std::unique_ptr<MemoryBuffer> StaticBinaryCache::getObject(StringRef Name) {
+std::unique_ptr<MemoryBuffer> StaticBinaryCache::getObject(StringRef Name) const {
   std::string CacheName;
   if (!getCacheFilename(Name, CacheName))
     return nullptr;
@@ -84,8 +133,27 @@ std::unique_ptr<MemoryBuffer> StaticBinaryCache::getObject(StringRef Name) {
   return MemoryBuffer::getMemBufferCopy(IRObjectBuffer.get()->getBuffer());
 }
 
+int StaticBinaryCache::getObjectFileDesc(StringRef Name) const {
+  std::string CacheName;
+  if (!getCacheFilename(Name, CacheName))
+    return -1;
+
+  // FIXME: Need to ensure we have an absolute path name so exec is not
+  // subject to name resolution by searching through a PATH.
+  DEBUG(dbgs() << "Exec cache file name: " << CacheName << "\n");
+
+  // Open the cache file in read-only mode and return the file descriptor.
+  int execFD = open(CacheName.c_str(), S_IRUSR/*perm: 00400*/);
+  if (execFD == -1) {		// Not an error: just a cache miss
+    DEBUG(dbgs() << "Executable does not exist in file cache "
+	  << CacheDir << "\n");
+  }
+  
+  return execFD;
+}
+
 bool StaticBinaryCache::getCacheFilename(StringRef ModID,
-                                         std::string &CacheName) {
+                                         std::string &CacheName) const {
   std::string Prefix("allexe:");
   size_t PrefixLength = Prefix.length();
   if (ModID.substr(0, PrefixLength) != Prefix)
@@ -100,7 +168,9 @@ bool StaticBinaryCache::getCacheFilename(StringRef ModID,
 #endif
   CacheName = CacheDir + CacheSubdir;
   size_t pos = CacheName.rfind('.');
-  CacheName.replace(pos, CacheName.length() - pos, ".o");
+  CacheName.replace(pos, CacheName.length() - pos, "");
+  DEBUG(dbgs() << "For Module " << ModID << 
+	" : Cache file name: " << CacheName << "\n");
   return true;
 }
 
--- tools/alley/StaticBinaryCache.h
+++ tools/alley/StaticBinaryCache.h
@@ -10,24 +10,47 @@
 
 namespace allvm {
 
+// ******************
+// FIXME: Cache interface should be indexed with the allexe file, not
+// LLVM::Module*, since this cache is the ALLVM cache for allexe -> native,
+// and an LLVM Module is just an intermediate (temporary) object along the
+// way.  For example, a cache hit should be possible to check without ever 
+// extracting the LLVM Module from the allexe.
+// ******************
+
 class StaticBinaryCache : public llvm::ObjectCache {
 public:
   ~StaticBinaryCache() override {}
 
   StaticBinaryCache(llvm::StringRef _CacheDir);
   StaticBinaryCache();
+
+  // Insert machine code for a given module into the cache.
+  // The machine code may come from a MemoryBuffer or a file.
   void notifyObjectCompiled(const llvm::Module *M,
                             llvm::MemoryBufferRef Obj) override;
+  void notifyObjectCompiled(const llvm::Module *M,
+                            const std::string& execFilePath);
+
+  // Retrieve machine code for a given module from the cache.
+  // Get this as either a MemoryBuffer object or a file descriptor.
+  // Both could be const but base class function for first is not marked const.
   std::unique_ptr<llvm::MemoryBuffer> getObject(const llvm::Module *M) override;
+  int getObjectFileDesc(const llvm::Module *M) const;
+  
+  // Query the cache to check whether the machine code for a module exists
   bool hasObjectFor(const llvm::Module *M);
+  
   static std::string generateName(llvm::StringRef Name, uint32_t crc,
                                   const CompilationOptions *Options = NULL);
 
 private:
   std::string CacheDir;
 
-  std::unique_ptr<llvm::MemoryBuffer> getObject(llvm::StringRef Name);
-  bool getCacheFilename(llvm::StringRef ModID, std::string &CacheName);
+  std::unique_ptr<llvm::MemoryBuffer> getObject(llvm::StringRef Name) const;
+  int getObjectFileDesc(llvm::StringRef Name) const;
+  bool getCacheFilename(llvm::StringRef ModID, std::string &CacheName) const;
+  std::string getAndResetCacheFile(const llvm::Module* M, bool doReset);
 };
 
 } // end namespace allvm
