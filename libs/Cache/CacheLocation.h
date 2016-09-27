535fda30c12f2c78e2f1c71792a71d3e93b65d35
Will Dietz <w@wdtz.org>
2016-09-27 15:16:59 -0500

    CacheLocation: Read ALLVM_CACHE_DIR env var for cache dir override.
    Mostly for testing purposes.

--- libs/Cache/CacheLocation.h
+++ libs/Cache/CacheLocation.h
@@ -10,8 +10,22 @@
 
 namespace allvm {
 
+static const auto CACHE_ENV_VAR = "ALLVM_CACHE_DIR";
+static const char *CacheDirEnvValue = std::getenv(CACHE_ENV_VAR);
+static const std::string CacheDirEnvOverride =
+    CacheDirEnvValue ? CacheDirEnvValue : "";
+
 static inline std::string getDefaultCacheDir(const llvm::Twine &CacheID) {
   llvm::SmallString<20> CacheDir;
+
+  // If user sets the environment variable, use it as cache directory
+  // (Note that we trust the user at this point, hopefully it's writable , etc.)
+  if (!CacheDirEnvOverride.empty()) {
+    CacheDir = CacheDirEnvOverride;
+    llvm::sys::path::append(CacheDir, CacheID);
+    return CacheDir.str();
+  }
+
   if (!llvm::sys::path::user_cache_directory(CacheDir, "allvm", CacheID))
     return ("allvm-cache-" + CacheID).str();
   return CacheDir.str();
