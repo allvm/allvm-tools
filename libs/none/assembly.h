c7d01b122068a96a61655e35d6223804b5054300
Will Dietz <w@wdtz.org>
2016-10-20 09:31:17 -0500

    assembly.h: Patch to disable exec stack on linux, not just GNU.
    Matches behavior used in compiler-rt, and same patch is applied
    to libunwind in allvm-nixpkgs.
    
    I'm not sure this is useful for our use or not but seems
    like we should use same libunwind bits throughout
    without a good reason otherwise :).

--- libs/none/assembly.h
+++ libs/none/assembly.h
@@ -47,7 +47,7 @@
 #define SYMBOL_IS_FUNC(name) .type name,@function
 #endif
 
-#if defined(__GNU__) || defined(__ANDROID__) || defined(__FreeBSD__)
+#if defined(__linux__) || defined(__ANDROID__) || defined(__FreeBSD__)
 #define NO_EXEC_STACK_DIRECTIVE .section .note.GNU-stack,"",%progbits
 #else
 #define NO_EXEC_STACK_DIRECTIVE
