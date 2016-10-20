c967e112610dd2c13f8aa664a8f67ab546f87c29
Will Dietz <w@wdtz.org>
2016-10-20 09:28:39 -0500

    Update libunwind bits to latest (r284125).
    git: 122a0f8114e58a294ee3b2bb1d4f9537ef7ed6df
    git-svn-id: https://llvm.org/svn/llvm-project/libunwind/trunk@284125

--- libs/none/assembly.h
+++ libs/none/assembly.h
@@ -19,7 +19,7 @@
 #if defined(__POWERPC__) || defined(__powerpc__) || defined(__ppc__)
 #define SEPARATOR @
 #elif defined(__arm64__)
-#define SEPARATOR % %
+#define SEPARATOR %%
 #else
 #define SEPARATOR ;
 #endif
@@ -30,32 +30,51 @@
 #define HIDDEN_DIRECTIVE .hidden
 #endif
 
-#define GLUE2(a, b) a##b
+#define GLUE2(a, b) a ## b
 #define GLUE(a, b) GLUE2(a, b)
 #define SYMBOL_NAME(name) GLUE(__USER_LABEL_PREFIX__, name)
 
 #if defined(__APPLE__)
+
 #define SYMBOL_IS_FUNC(name)
+#define NO_EXEC_STACK_DIRECTIVE
+
 #elif defined(__ELF__)
+
 #if defined(__arm__)
-#define SYMBOL_IS_FUNC(name) .type name, % function
+#define SYMBOL_IS_FUNC(name) .type name,%function
 #else
-#define SYMBOL_IS_FUNC(name) .type name, @function
+#define SYMBOL_IS_FUNC(name) .type name,@function
 #endif
+
+#if defined(__GNU__) || defined(__ANDROID__) || defined(__FreeBSD__)
+#define NO_EXEC_STACK_DIRECTIVE .section .note.GNU-stack,"",%progbits
 #else
+#define NO_EXEC_STACK_DIRECTIVE
+#endif
+
+#else
+
 #define SYMBOL_IS_FUNC(name)                                                   \
-  .def name SEPARATOR.scl 2 SEPARATOR.type 32 SEPARATOR.endef
+  .def name SEPARATOR                                                          \
+    .scl 2 SEPARATOR                                                           \
+    .type 32 SEPARATOR                                                         \
+  .endef
+
+#define NO_EXEC_STACK_DIRECTIVE
+
 #endif
 
-#define DEFINE_LIBUNWIND_FUNCTION(name)                                        \
-  .globl SYMBOL_NAME(name) SEPARATOR SYMBOL_IS_FUNC(SYMBOL_NAME(name))         \
-      SEPARATOR SYMBOL_NAME(name)                                              \
-      :
+#define DEFINE_LIBUNWIND_FUNCTION(name)                   \
+  .globl SYMBOL_NAME(name) SEPARATOR                      \
+  SYMBOL_IS_FUNC(SYMBOL_NAME(name)) SEPARATOR             \
+  SYMBOL_NAME(name):
 
-#define DEFINE_LIBUNWIND_PRIVATE_FUNCTION(name)                                \
-  .globl SYMBOL_NAME(name) SEPARATOR HIDDEN_DIRECTIVE SYMBOL_NAME(name)        \
-      SEPARATOR SYMBOL_IS_FUNC(SYMBOL_NAME(name)) SEPARATOR SYMBOL_NAME(name)  \
-      :
+#define DEFINE_LIBUNWIND_PRIVATE_FUNCTION(name)           \
+  .globl SYMBOL_NAME(name) SEPARATOR                      \
+  HIDDEN_DIRECTIVE SYMBOL_NAME(name) SEPARATOR            \
+  SYMBOL_IS_FUNC(SYMBOL_NAME(name)) SEPARATOR             \
+  SYMBOL_NAME(name):
 
 #if defined(__arm__)
 #if !defined(__ARM_ARCH)
