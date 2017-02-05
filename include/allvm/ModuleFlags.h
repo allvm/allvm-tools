d44fe7ce6d2980840481dcc3a143b4a248ee2588
Will Dietz <w@wdtz.org>
2017-02-05 15:36:46 -0600

    ModuleFlags: update-format, whoops.

--- include/allvm/ModuleFlags.h
+++ include/allvm/ModuleFlags.h
@@ -1,19 +1,20 @@
 #ifndef ALLVM_MODULEFLAGS_H
 #define ALLVM_MODULEFLAGS_H
 
-#include <llvm/IR/Module.h>
 #include <llvm/ADT/StringRef.h>
 #include <llvm/IR/Metadata.h>
+#include <llvm/IR/Module.h>
 
 namespace allvm {
 
 static const llvm::StringRef MF_ALLVM_SOURCE = "ALLVM Source";
 static const llvm::StringRef MF_WLLVM_SOURCE = "WLLVM Source";
 
-inline void setModuleFlag(llvm::Module *M, llvm::StringRef Key, llvm::StringRef Source) {
-	llvm::LLVMContext &C = M->getContext();
-	M->addModuleFlag(llvm::Module::AppendUnique, Key, llvm::MDNode::get(C,
-llvm::MDString::get(C, Source)));
+inline void setModuleFlag(llvm::Module *M, llvm::StringRef Key,
+                          llvm::StringRef Source) {
+  llvm::LLVMContext &C = M->getContext();
+  M->addModuleFlag(llvm::Module::AppendUnique, Key,
+                   llvm::MDNode::get(C, llvm::MDString::get(C, Source)));
 }
 
 inline void setWLLVMSource(llvm::Module *M, llvm::StringRef Source) {
@@ -27,4 +28,3 @@ inline void setALLVMSource(llvm::Module *M, llvm::StringRef Source) {
 } // end namespace allvm
 
 #endif // ALLVM_MODULEFLAGS_H
-
