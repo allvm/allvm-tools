519ce7d6866f8ffea8f6b5b314c742ddac8b1dab
Will Dietz <w@wdtz.org>
2017-06-28 17:33:41 -0500

    get-llvm-lit script: tweak for using latest lit, patch for output fix

--- test/get-llvm-lit.sh
+++ test/get-llvm-lit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=3.9.0
+REV=70e6b37768d55ddff56e65b2ca8813544cca15e3
 
 ROOT=$(readlink -f $(dirname $0))
 UTIL_DIR=$ROOT/../utils
@@ -12,10 +12,17 @@ trap 'rm -rf "$dir"' EXIT
 
 cd $dir
 
-curl http://llvm.org/releases/${VERSION}/llvm-${VERSION}.src.tar.xz | tar xJvf - llvm-${VERSION}.src/utils/lit
+curl -L https://github.com/llvm-mirror/llvm/archive/${REV}.tar.gz | tar xzvf - --wildcards "llvm-*/utils/lit"
 
-mv llvm-${VERSION}.src/utils $UTIL_DIR
+POSIXLY_CORRECT=1 patch -p3 -i $ROOT/D34732.diff -d llvm-*/utils/lit
+
+mv llvm-*/utils $UTIL_DIR
 
 # Remove things that only take up space since we don't use them
-rm -rf $UTIL_DIR/lit/examples
-rm -rf $UTIL_DIR/lit/tests
+rm -rf $UTIL_DIR/lit/{examples,tests,lit/ExampleTests.*}
+
+cd $UTIL_DIR
+git rm -rf --cached .
+git add -f .
+
+git commit -av -m "lit: Update to (patched) copy from LLVM ${REV}"
