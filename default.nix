a5ebd2bd738125c301ab8678a968e600483f25c8
Will Dietz <w@wdtz.org>
2017-09-19 09:33:38 -0500

    default.nix: Use LLVM5 from newer nixos-unstable

--- default.nix
+++ default.nix
@@ -3,10 +3,10 @@
 with import nixpkgs {};
 {
   allvm-tools-gcc = callPackage ./nix/build.nix {
-    inherit (llvmPackages_4) llvm clang lld;
+    inherit (llvmPackages_5) llvm clang lld;
   };
 
   allvm-tools-clang = callPackage ./nix/build.nix {
-    inherit (llvmPackages_4) stdenv llvm clang lld;
+    inherit (llvmPackages_5) stdenv llvm clang lld;
   };
 }
