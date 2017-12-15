c684cbdcf9c3b5d7fe1dd97d90c6ecd7c940e966
Will Dietz <w@wdtz.org>
2017-12-15 15:00:07 -0600

    move build.nix into /nix subdir

--- default.nix
+++ default.nix
@@ -2,11 +2,11 @@
 
 with import nixpkgs {};
 {
-  allvm-tools-gcc = callPackage ./build.nix {
+  allvm-tools-gcc = callPackage ./nix/build.nix {
     inherit (llvmPackages_4) llvm clang lld;
   };
 
-  allvm-tools-clang = callPackage ./build.nix {
+  allvm-tools-clang = callPackage ./nix/build.nix {
     inherit (llvmPackages_4) stdenv llvm clang lld;
   };
 }
