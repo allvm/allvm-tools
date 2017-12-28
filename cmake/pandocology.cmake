edd3baffce7154aec7d28380841bd4d353c23de3
Will Dietz <w@wdtz.org>
2017-12-28 15:28:48 -0600

    pandocology: re-disable detection of pandoc, make this optional

--- cmake/pandocology.cmake
+++ cmake/pandocology.cmake
@@ -40,15 +40,16 @@
 
 include(CMakeParseArguments)
 
-if(NOT EXISTS ${PANDOC_EXECUTABLE})
-    # find_program(PANDOC_EXECUTABLE NAMES pandoc)
-    find_program(PANDOC_EXECUTABLE pandoc)
-    mark_as_advanced(PANDOC_EXECUTABLE)
-    if(NOT EXISTS ${PANDOC_EXECUTABLE})
-        message(FATAL_ERROR "Pandoc not found. Install Pandoc (http://johnmacfarlane.net/pandoc/) or set cache variable PANDOC_EXECUTABLE.")
-        return()
-    endif()
-endif()
+## XXX: We detect pandoc ourselves as an optional dependency
+##  if(NOT EXISTS ${PANDOC_EXECUTABLE})
+##      # find_program(PANDOC_EXECUTABLE NAMES pandoc)
+##      find_program(PANDOC_EXECUTABLE pandoc)
+##      mark_as_advanced(PANDOC_EXECUTABLE)
+##      if(NOT EXISTS ${PANDOC_EXECUTABLE})
+##          message(FATAL_ERROR "Pandoc not found. Install Pandoc (http://johnmacfarlane.net/pandoc/) or set cache variable PANDOC_EXECUTABLE.")
+##          return()
+##      endif()
+##  endif()
 
 ###############################################################################
 # Based on code from UseLATEX
