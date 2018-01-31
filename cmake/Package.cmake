1764801f748c446e96a5616215fcd662b67c520c
Will Dietz <w@wdtz.org>
2018-01-31 14:17:18 -0600

    cmake: create config with variables pointing to dirs, include exported

--- cmake/Package.cmake
+++ cmake/Package.cmake
@@ -1,4 +1,9 @@
-set(CMAKE_CONFIG_DEST "lib/cmake/allvm")
+
+set(ALLVM_INCLUDE_INSTALL_DIR include)
+set(ALLVM_LIB_INSTALL_DIR lib)
+set(ALLVM_PREFIX .)
+
+set(CMAKE_CONFIG_DEST "${ALLVM_LIB_INSTALL_DIR}/cmake/allvm")
 
 macro(add_allvm_library name)
   llvm_add_library(${name} ${ARGN})
@@ -13,6 +18,19 @@ endmacro(add_allvm_library name)
 
 install(
   EXPORT ALLVM
-  FILE ALLVMConfig.cmake
+  FILE ALLVMTargets.cmake
   DESTINATION ${CMAKE_CONFIG_DEST}
+)
+
+include(CMakePackageConfigHelpers)
+
+configure_package_config_file(
+  cmake/ALLVMConfig.cmake.in
+  ${CMAKE_CURRENT_BINARY_DIR}/ALLVMConfig.cmake
+  INSTALL_DESTINATION ${ALLVM_LIB_INSTALL_DIR}/cmake/allvm
+  PATH_VARS ALLVM_INCLUDE_INSTALL_DIR ALLVM_LIB_INSTALL_DIR ALLVM_PREFIX
+)
+install(
+  FILES ${CMAKE_CURRENT_BINARY_DIR}/ALLVMConfig.cmake
+  DESTINATION ${ALLVM_LIB_INSTALL_DIR}/cmake/allvm
 )
