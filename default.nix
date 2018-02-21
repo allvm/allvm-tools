2e574e59799c326f2aa1568f5258c6a0d70679e9
Will Dietz <w@wdtz.org>
2018-02-20 19:08:17 -0600

    kludge: try using aarch64-multiplatform-musl

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { nixpkgs ? import ./nix/fetch-nixpkgs.nix }:
 
-with import nixpkgs {};
+with import nixpkgs { localSystem = (import (nixpkgs + "/lib")).systems.examples.aarch64-multiplatform-musl; };
 {
   allvm-tools-gcc = callPackage ./nix/build.nix {
     inherit (llvmPackages_5) llvm clang lld;
