680dfd817704f5bfa164f80db4eda76482bda026
Will Dietz <w@wdtz.org>
2018-02-23 11:49:04 -0600

    tweak nix pinning code to fix hydra restricted eval
    As suggested on upstream issue.
    
    Downside is on 1.11 the fetch is unchecked, but oh well.

--- nix/fetch-nixpkgs.nix
+++ nix/fetch-nixpkgs.nix
@@ -1,23 +1,10 @@
 let
   spec = builtins.fromJSON (builtins.readFile ./nixpkgs-src.json);
-  src = import <nix/fetchurl.nix> {
+  fetchTarball = if builtins.lessThan builtins.nixVersion "1.12" then
+    { url, sha256 }: builtins.fetchTarball url
+    else builtins.fetchTarball;
+in
+  fetchTarball {
     url = "https://github.com/${spec.owner}/${spec.repo}/archive/${spec.rev}.tar.gz";
-    inherit (spec) sha256;
-  };
-  nixcfg = import <nix/config.nix>;
-in builtins.derivation {
-  system = builtins.currentSystem;
-  name = "${src.name}-unpacked";
-  builder = builtins.storePath nixcfg.shell;
-  inherit src;
-  args = [
-    (builtins.toFile "builder" ''
-      $coreutils/mkdir $out
-      cd $out
-      $gzip -d < $src | $tar -x --strip-components=1
-    '')
-  ];
-  coreutils = builtins.storePath nixcfg.coreutils;
-  tar = builtins.storePath nixcfg.tar;
-  gzip = builtins.storePath nixcfg.gzip;
-}
+    sha256 = spec.sha256;
+  }
