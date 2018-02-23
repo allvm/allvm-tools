b8870d5a261a1cccd39505e68f8aeec4d233f702
Will Dietz <w@wdtz.org>
2018-02-23 17:28:27 -0600

    nix/*: bump to recent nixpkgs master, don't bother with 1.11 compat

--- nix/fetch-nixpkgs.nix
+++ nix/fetch-nixpkgs.nix
@@ -1,8 +1,5 @@
 let
   spec = builtins.fromJSON (builtins.readFile ./nixpkgs-src.json);
-  fetchTarball = if builtins.lessThan builtins.nixVersion "1.12" then
-    { url, sha256 }: builtins.fetchTarball url
-    else builtins.fetchTarball;
 in
   fetchTarball {
     url = "https://github.com/${spec.owner}/${spec.repo}/archive/${spec.rev}.tar.gz";
