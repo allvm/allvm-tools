0c6e914dd95a5bdfe54ccfaa1129726c699a1184
Will Dietz <w@wdtz.org>
2018-04-03 11:37:24 -0500

    travis: experiment with local binary cache

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -4,7 +4,7 @@
 # Also, ensure sandbox support is enabled
 mkdir -p ~/.config/nix
 cat > ~/.config/nix/nix.conf <<EOF
-substituters = https://cache.nixos.org https://cache.allvm.org
+substituters = https://cache.nixos.org https://cache.allvm.org file:///nix-cache
 trusted-public-keys = gravity.cs.illinois.edu-1:yymmNS/WMf0iTj2NnD0nrVV8cBOXM9ivAkEdO1Lro3U= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
 sandbox = true
 EOF
