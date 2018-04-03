6a1bd909032940b0ebd7c5ae5d0b7206f13da49a
Will Dietz <w@wdtz.org>
2018-04-03 10:10:11 -0500

    shell.nix: fix attribute name, whoops

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 { ... } @ args:
 # Pick single attr, but disable Werror flags since they
 # are likely annoying during development.
-(import ./default.nix args).allvm-tools-clang.override { useClangWerrorFlags = false; }
+(import ./default.nix args).allvm-tools.override { useClangWerrorFlags = false; }
