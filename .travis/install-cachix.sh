9a479a7cdacaed711fc504721abb711784807098
Will Dietz <w@wdtz.org>
2018-06-17 14:54:53 -0500

    travis: fix quoting

--- .travis/install-cachix.sh
+++ .travis/install-cachix.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nix-env -if https://github.com/cachix/cachix/tarball/master --extra-substituters https://cachix.cachix.org --trusted-public-keys 'cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
+nix-env -if https://github.com/cachix/cachix/tarball/master --extra-substituters https://cachix.cachix.org --trusted-public-keys 'cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY='
