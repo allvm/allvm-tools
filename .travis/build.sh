a2fe5dec82959bcf98a08ec5920598fce30e8816
Will Dietz <w@wdtz.org>
2018-06-27 18:38:36 -0500

    travis/build.sh: push build deps, push even if already built

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,4 +2,7 @@
 
 cachix push allvm --watch-store &
 
-nix-build | cachix push allvm
+nix-build -o result | cachix push allvm
+
+# Push build deps, alt outputs, and ensure paths are pushed if already on allvm.cache.org
+nix-store -qR --include-outputs $(nix-store -q --deriver ./result) | cachix push allvm
