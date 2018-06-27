b2db4772ed28542cf9944a36e077961437aafa01
Will Dietz <w@wdtz.org>
2018-06-27 18:55:41 -0500

    travis/build: drop --watch-store, realize then push

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-cachix push allvm --watch-store &
-
 nix-build -o result | cachix push allvm
 
 # Push build deps, alt outputs, and ensure paths are pushed if already on allvm.cache.org
-nix-store -qR --include-outputs $(nix-store -q --deriver ./result) | cachix push allvm
+PATHS="$(nix-store -qR --include-outputs $(nix-store -q --deriver ./result))"
+
+echo "Fetching all build deps..."
+nix-store -r $PATHS
+
+echo "Pushing all build deps explicitly..."
+echo $PATHS | cachix push allvm
