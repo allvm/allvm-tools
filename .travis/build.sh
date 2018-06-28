68a1019750fc25c0725c405b5def9d87fe1df34e
Will Dietz <w@wdtz.org>
2018-06-27 19:15:16 -0500

    build.sh: don't download entire build closure every single build

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
+cachix push allvm --watch-store &
+
 nix-build -o result | cachix push allvm
 
-# Push build deps, alt outputs, and ensure paths are pushed if already on allvm.cache.org
-PATHS="$(nix-store -qR --include-outputs $(nix-store -q --deriver ./result))"
-
-echo "Fetching all build deps..."
-nix-store -r $PATHS
-
-echo "Pushing all build deps explicitly..."
-echo $PATHS | cachix push allvm
+cachix push allvm ./result
