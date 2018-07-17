a16e7dc08c93f7279649862ec59b65cbb2021cad
Will Dietz <w@wdtz.org>
2018-07-17 15:37:24 -0500

    nixpkgs-src: bump to latest master (2018-07-17)

--- nix/nixpkgs-src.nix
+++ nix/nixpkgs-src.nix
@@ -1,7 +1,7 @@
 {
   owner  = "NixOS";
   repo = "nixpkgs";
-  rev = "00db477324bf58571274935e70fb392cdf5c3736";
-  sha256 = "1rxc8z9qdavhf8gbli56ybrnrabvvz7nvgp0msrwib49cjipnbkm";
+  rev = "90223e4a4a60556e7b706c69480593557b8a2fb9";
+  sha256 = "1avlp50j69bi5clijccg2jmd560ygzkjfdwmadcjpsiwzhd7wdnp";
 }
 
