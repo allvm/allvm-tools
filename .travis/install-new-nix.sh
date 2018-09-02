5c624399a9d93d90d9f413a937f57d2f64188f9b
Will Dietz <w@wdtz.org>
2018-09-01 20:48:00 -0500

    travis/install-new-nix: bump 6204 -> 6377

--- .travis/install-new-nix.sh
+++ .travis/install-new-nix.sh
@@ -2,4 +2,4 @@
 
 # Install recent-ish Nix for misc fixes.
 # Attempt to "update" to it, for the day it's no longer "new".
-nix-env -u /nix/store/ph2xyhvrcjpvj8s6my8ylz32qh7kaz7h-nix-2.1pre6204_966407bc
+nix-env -u /nix/store/4kfzxnzwdi66z491r12lcarmgwxy9awz-nix-2.1pre6377_954d1f4d
