bb60144546e2f3f0b22baa75f33a9622726ff90c
Will Dietz <w@wdtz.org>
2018-09-04 21:33:04 -0500

    nixpkgs: latest staging

--- nix/nixpkgs-src.nix
+++ nix/nixpkgs-src.nix
@@ -1,7 +1,7 @@
 {
   owner  = "NixOS";
   repo = "nixpkgs";
-  rev = "0fa04d646d65152d70e32f75d5f2a9518dce176e";
-  sha256 = "1y0y63qj5kd88cc0hnk0blxfb0bpfgxddfs6lcm22m5s3a7vdhda";
+  rev = "d93aa1c50fc8a83be1c709f905d8c94e1677845f";
+  sha256 = "105kjhm8zfzns7yiz0h37kcaa99c6i4vr559685pk7lq7q50mdph";
 }
 
