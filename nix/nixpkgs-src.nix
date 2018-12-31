b737e5ec0224c812275e6ae5d573ab0844fbb4b7
Will Dietz <w@wdtz.org>
2018-12-30 22:55:34 -0600

    nixpkgs-src: bump to latest for LLVM 7.0.1, gcc 7.4, more
    Uses my fork just because it's what I know works,
    upstream will likely be a better choice once updates
    are included and stabilized.

--- nix/nixpkgs-src.nix
+++ nix/nixpkgs-src.nix
@@ -1,7 +1,7 @@
 {
   owner  = "dtzWill";
   repo = "nixpkgs";
-  rev = "f02c8e29d7b03e58dfd403e1f916aa730658a1d5";
-  sha256 = "0pzlibzhz60whj3q4a8flqnwg5r373lvi6jiik7adccaglgqb6hv";
+  rev = "nixos-dtz-2018-12-30-3";
+  sha256 = "159vpb8hfn1iv4h7pavnc22pyg31b7hn8mjj4dy7cyh6ygql9af7";
 }
 
