935ca0ec38e9e50b297e9d5095521e662861cfac
Will Dietz <w@wdtz.org>
2019-01-01 14:59:29 -0600

    llvm7ification

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,6 +1,6 @@
 self: super: rec {
   allvm-tools = super.callPackage ./build.nix {
-    inherit (self.llvmPackages_6) llvm clang lld;
+    inherit (self.llvmPackages_7) llvm clang lld;
   };
 
   allvm-tools-variants = super.recurseIntoAttrs {
