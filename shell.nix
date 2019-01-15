e8e3dd07c657a79c0232714417c60b80c8b65353
Will Dietz <w@wdtz.org>
2019-01-15 15:03:38 -0600

    hmm, see how things go w/gcc8

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 { ... } @ args:
 # Pick single attr, but disable Werror flags since they
 # are likely annoying during development.
-(import ./default.nix args).allvm-tools.override { useClangWerrorFlags = true; }
+(import ./default.nix args).allvm-tools.override { useClangWerrorFlags = false; }
