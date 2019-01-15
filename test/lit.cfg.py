7f8c9ce9f4d7eb0b56daefbe54425da3bd54107c
Will Dietz <w@wdtz.org>
2019-01-15 16:24:33 -0600

    wip

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -1,4 +1,4 @@
-# -*- Python -*-
+# vim: set filetype=python:
 
 import os
 import platform
@@ -14,81 +14,87 @@ from lit.llvm import llvm_config
 # Configuration file for the 'lit' test runner.
 
 # name: The name of this test suite.
-config.name = 'lld'
+config.name = 'allvm'
 
 # testFormat: The test format to use to interpret tests.
-#
-# For now we require '&&' between commands, until they get globally killed and
-# the test runner updated.
-config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
+config.test_format = lit.formats.ShTest(True) # not llvm_config.use_lit_shell)
 
 # suffixes: A list of file extensions to treat as test files.
-config.suffixes = ['.ll', '.s', '.test', '.yaml', '.objtxt']
+config.suffixes = ['.ll', '.c', '.cpp', '.test']
 
 # excludes: A list of directories to exclude from the testsuite. The 'Inputs'
 # subdirectories contain auxiliary inputs for various tests in their parent
 # directories.
-config.excludes = ['Inputs']
+config.excludes = [ "Inputs", "source" ]
 
 # test_source_root: The root path where tests are located.
 config.test_source_root = os.path.dirname(__file__)
 
-config.test_exec_root = os.path.join(config.lld_obj_root, 'test')
+config.test_exec_root = os.path.join(config.allvm_obj_root, 'test')
 
 llvm_config.use_default_substitutions()
-llvm_config.use_lld()
+# llvm_config.use_lld()
 
-tool_patterns = [
-    'llc', 'llvm-as', 'llvm-mc', 'llvm-nm', 'llvm-objdump', 'llvm-pdbutil',
-    'llvm-dwarfdump', 'llvm-readelf', 'llvm-readobj', 'obj2yaml', 'yaml2obj',
-    'opt', 'llvm-dis']
+#tool_patterns = [
+#    'llc', 'llvm-as', 'llvm-mc', 'llvm-nm', 'llvm-objdump', 'llvm-pdbutil',
+#    'llvm-dwarfdump', 'llvm-readelf', 'llvm-readobj', 'obj2yaml', 'yaml2obj',
+#    'opt', 'llvm-dis']
+#
+# llvm_config.add_tool_substitutions(tool_patterns)
 
-llvm_config.add_tool_substitutions(tool_patterns)
+# Propagate some variables from the host environment.
+#llvm_config.with_system_environment(
+#    ['HOME', 'INCLUDE', 'LIB', 'TMP', 'TEMP', 'ASAN_SYMBOLIZER_PATH', 'MSAN_SYMBOLIZER_PATH'])
+
+#newpath = config.allvm_tools_dir "@LLVM_RUNTIME_OUTPUT_INTDIR@:@LLVM_TOOLS_BINARY_DIR@"
+#curpath = os.environ.get('PATH', '')
+#if curpath:
+#    newpath += ":" + curpath
+#config.environment['PATH'] = newpath
+config.environment['XDG_CACHE_HOME'] = config.test_exec_root + "/cache";
+
+# TODO: Get this more reliably/directly: " = @LIBNONE_PATH@" or something
+libnone = "@LLVM_LIBRARY_OUTPUT_INTDIR@/libnone.a"
+
+config.substitutions.append( ('%libnone', libnone) )
+config.substitutions.append( ('%alley', "@LLVM_RUNTIME_OUTPUT_INTDIR@/alley") )
+
+#libpath = os.environ.get('LD_LIBRARY_PATH', '')
+#if libpath:
+#    libpath = ":" + libpath
+#config.environment['LD_LIBRARY_PATH'] = "@OBJDIR@/prefix/lib" + libpath
 
 # When running under valgrind, we mangle '-vg' onto the end of the triple so we
 # can check it with XFAIL and XTARGET.
-if lit_config.useValgrind:
-    config.target_triple += '-vg'
+#if lit_config.useValgrind:
+#    config.target_triple += '-vg'
 
 # Running on ELF based *nix
-if platform.system() in ['FreeBSD', 'Linux']:
-    config.available_features.add('system-linker-elf')
+#if platform.system() in ['FreeBSD', 'Linux']:
+#    config.available_features.add('system-linker-elf')
 
 # Set if host-cxxabi's demangler can handle target's symbols.
-if platform.system() not in ['Windows']:
-    config.available_features.add('demangler')
+#if platform.system() not in ['Windows']:
+#    config.available_features.add('demangler')
 
-llvm_config.feature_config(
-    [('--build-mode', {'DEBUG': 'debug'}),
-     ('--assertion-mode', {'ON': 'asserts'}),
-     ('--targets-built', {'AArch64': 'aarch64',
-                          'AMDGPU': 'amdgpu',
-                          'ARM': 'arm',
-                          'AVR': 'avr',
-                          'Hexagon': 'hexagon',
-                          'Mips': 'mips',
-                          'PowerPC': 'ppc',
-                          'Sparc': 'sparc',
-                          'WebAssembly': 'wasm',
-                          'X86': 'x86'})
-     ])
+#llvm_config.feature_config(
+#    [('--build-mode', {'DEBUG': 'debug'}),
+#     ('--assertion-mode', {'ON': 'asserts'}),
+#     ('--targets-built', {'AArch64': 'aarch64',
+#                          'AMDGPU': 'amdgpu',
+#                          'ARM': 'arm',
+#                          'AVR': 'avr',
+#                          'Hexagon': 'hexagon',
+#                          'Mips': 'mips',
+#                          'PowerPC': 'ppc',
+#                          'Sparc': 'sparc',
+#                          'WebAssembly': 'wasm',
+#                          'X86': 'x86'})
+#     ])
 
 # Set a fake constant version so that we get consistent output.
 config.environment['LLD_VERSION'] = 'LLD 1.0'
 config.environment['LLD_IN_TEST'] = '1'
-
-# Indirectly check if the mt.exe Microsoft utility exists by searching for
-# cvtres, which always accompanies it.  Alternatively, check if we can use
-# libxml2 to merge manifests.
-if (lit.util.which('cvtres', config.environment['PATH'])) or \
-        (config.llvm_libxml2_enabled == '1'):
-    config.available_features.add('manifest_tool')
-
-if (config.llvm_libxml2_enabled == '1'):
-    config.available_features.add('libxml2')
-
-if config.have_dia_sdk:
-    config.available_features.add("diasdk")
 
 tar_executable = lit.util.which('tar', config.environment['PATH'])
 if tar_executable:
