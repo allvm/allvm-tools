f6cd14d8f835c46fa8e4ae037d19e717d1cef0b8
Will Dietz <w@wdtz.org>
2019-01-15 21:50:28 -0600

    idk

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,6 +1,6 @@
 self: super: rec {
   allvm-tools = super.callPackage ./build.nix {
-    inherit (self.llvmPackages_7) llvm lld;
+    inherit (self.llvmPackages_7) stdenv llvm clang lld;
   };
 
   allvm-tools-variants = super.recurseIntoAttrs {
