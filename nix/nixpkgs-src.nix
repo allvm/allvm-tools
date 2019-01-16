c9a2b6c5848912bb640e3725132f1637899221c9
Will Dietz <w@wdtz.org>
2019-01-16 16:46:25 -0600

    nixpkgs: bump to today's tag

--- nix/nixpkgs-src.nix
+++ nix/nixpkgs-src.nix
@@ -1,7 +1,7 @@
 {
   owner  = "dtzWill";
   repo = "nixpkgs";
-  rev = "ddd61e69cc321ab1513532e7aac18f6d4f044416";
-  sha256 = "1ar1154485pqjf5cawba48nk1ix557w94ilj7kldd6wv75iradlh";
+  rev = "nixos-dtz-2019-01-16";
+  sha256 = "1rgbcdd14849a8vz2rhxrcwb7nd8k1v1cn4j0d90hnjdrqkpacly";
 }
 
