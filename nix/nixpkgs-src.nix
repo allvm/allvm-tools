0747643cb09d5b5d94c3b43aba26d5cfedb3675c
Will Dietz <w@wdtz.org>
2019-10-28 03:08:47 -0500

    nixpkgs-src: 2019-09-16-3 -> 2019-10-27
    Among other updates, musl 1.1.24

--- nix/nixpkgs-src.nix
+++ nix/nixpkgs-src.nix
@@ -1,7 +1,7 @@
 {
   owner  = "dtzWill";
   repo = "nixpkgs";
-  rev = "nixos-dtz-2019-09-16-3";
-  sha256 = "1wr14mwp9d80asdzv1c2jkpdxcl8fljq8f8l72rdscvb0bi42p6q";
+  rev = "nixos-dtz-2019-10-27";
+  sha256 = "04pqvhw52d9hbfrgbvhxkiv8ici7qashjhb6ircj3blhabrnybg3";
 }
 
