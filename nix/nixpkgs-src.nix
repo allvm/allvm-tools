273c095ba325e5eeb458f385f5ead46f9f4f67cf
Will Dietz <w@wdtz.org>
2019-10-28 21:42:16 -0500

    nixpkgs-src: minor bump

--- nix/nixpkgs-src.nix
+++ nix/nixpkgs-src.nix
@@ -1,7 +1,6 @@
 {
   owner  = "dtzWill";
   repo = "nixpkgs";
-  rev = "nixos-dtz-2019-10-27";
-  sha256 = "04pqvhw52d9hbfrgbvhxkiv8ici7qashjhb6ircj3blhabrnybg3";
+  rev = "nixos-dtz-2019-10-28-2";
+  sha256 = "1qgbnmx0ydjqri40x7kzr12aj9zy30bdwals1kx6mdkp7s3s8fhz";
 }
-
