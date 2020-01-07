0b8507ad2830a2b2421a12bab1f203db0bd00f68
Will Dietz <w@wdtz.org>
2020-01-07 02:02:18 -0600

    bump for 2020 packages fixes

--- nix/nixpkgs-src.nix
+++ nix/nixpkgs-src.nix
@@ -1,6 +1,6 @@
 {
   owner  = "NixOS";
   repo = "nixpkgs";
-  rev = "3fb22f785aed5d7b6946815ef1a8f99ec300c382"; # 2020-01-01
-  sha256 = "06ibfxy98bxr1ylsz9mzk4b5kfh39cav7p2d4k3rnkhr83i2k0bk";
+  rev = "f55ec6fd6952d9f6ecb1a00d9d8ffca92e523a9f"; # 2020-01-07
+  sha256 = "0mblim6qzdrsc9xkgvlwin2zb2a3wb58bj2fp0hsiyss2g5b1vpb";
 }
